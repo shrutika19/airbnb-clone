0c809d8324f2cbb781fd0208caa9d74965b2ec00
Shrutika Jaiswal <jaiswalshrutika19@gmail.com>
2024-03-19 08:34:15 +0530

    first commit

---
+++ api/index.js
@@ -0,0 +1,44 @@
+const express = require('express');
+var cors = require('cors');
+const mongoose = require('mongoose');
+const User = require('./models/User.js');
+const bcrypt = require('bcryptjs')
+require('dotenv').config()
+
+const bcryptSalt = bcrypt.genSaltSync(10);
+
+const app = express();
+app.use(cors({
+    credentials: true,
+    origin: 'http://localhost:5173'
+}));
+
+app.use(express.json());
+
+mongoose.connect(process.env.MONGO_URL);
+
+
+app.get('/', (req, res) => {
+    res.send('Hello, World!');
+});
+
+app.get('/test', (req, res) => {
+    res.send('Hello, World! testing');
+});
+
+app.post('/register', async (req, res) => {
+    const { name, email, password } = req.body;
+    const userDoc = await User.create({
+        name,
+        email,
+        password: bcrypt.hashSync(password, bcryptSalt)
+    });
+
+    res.json(userDoc)
+
+})
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server is listening on port ${PORT}`);
+});
---
+++ client/src/App.jsx
@@ -0,0 +1,24 @@
+import { Route, Routes } from 'react-router-dom'
+import './App.css'
+import IndexPage from './pages/IndexPage'
+import LoginPage from './pages/LoginPage'
+import Layout from './components/Layout'
+import RegisterPage from './pages/RegisterPage'
+import axios from 'axios'
+
+axios.defaults.baseURL = 'http://localhost:3000/'
+
+function App() {
+
+  return (
+    <Routes>
+      <Route path='/' element={<Layout />}>
+        <Route index element={<IndexPage />} />
+        <Route path='/login' element={<LoginPage />} />
+        <Route path='/register' element={<RegisterPage />} />
+      </Route>
+    </Routes>
+  )
+}
+
+export default App
