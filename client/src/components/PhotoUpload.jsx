76085558fd300c8d1c246d3e7519d8f98ff9b3b8
Shrutika Jaiswal <jaiswalshrutika19@gmail.com>
2024-03-26 11:01:22 +0530

    implemented delete image functionality

--- client/src/components/PhotoUpload.jsx
+++ client/src/components/PhotoUpload.jsx
@@ -30,6 +30,10 @@ const PhotoUpload = ({ addedPhotos, onChange }) => {
         })
     }
 
+    const removePhoto = (imageUrl) => {
+        onChange([...addedPhotos.filter(image => image !== imageUrl)])
+    }
+
     return (
         <div>
             <div className='flex gap-2'>
@@ -44,8 +48,14 @@ const PhotoUpload = ({ addedPhotos, onChange }) => {
             <div className='mt-2 grid gap-2 grid-cols-3 md:grid-cols-4 lg:grid-cols-6'>
                 {
                     addedPhotos.length > 0 && addedPhotos.map((link, index) => (
-                        <div className='h-32 flex' key={index}>
+                        <div className='h-32 flex relative' key={index}>
                             <img className='rounded-2xl w-full object-cover' src={'http://localhost:3000/uploads/' + link} />
+                            <button onClick={() => removePhoto(link)} className='absolute bottom-1 right-1 text-white p-1 bg-black bg-opacity-50 rounded-full' title='Delete image'>
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-4 h-4">
+                                    <path strokeLinecap="round" strokeLinejoin="round" d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" />
+                                </svg>
+
+                            </button>
                         </div>
                     ))
                 }
