844b5257e3a266efd32178643acaf529b5455a63
Shrutika Jaiswal <jaiswalshrutika19@gmail.com>
2024-04-10 14:55:02 +0530

    added booking functionality

--- client/src/pages/BookingPage.jsx
+++ client/src/pages/BookingPage.jsx
@@ -1,10 +1,42 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom';
+import axios from 'axios';
+import AddressLink from '../components/AddressLink';
+import BookingDates from '../components/BookingDates';
 
 const BookingPage = () => {
     const { id } = useParams();
+    const [booking, setBooking] = useState(null);
+    useEffect(() => {
+        if (id) {
+            axios.get('/bookings').then(response => {
+                const foundBooking = response.data.find(({ _id }) => _id === id)
+                if (foundBooking) {
+                    setBooking(foundBooking);
+                }
+            })
+        }
+    }, [id]);
+
+    if (!booking) {
+        return '';
+    }
+
     return (
-        <div>BookingPage: {id}</div>
+        <div className='my-8'>
+            <h1 className='text-2xl'>{booking.place.title}</h1>
+            <AddressLink  >{booking.place.address} </AddressLink>
+            <div className='bg-gray-200 p-6 my-6 rounded-2xl flex items-center justify-between'>
+                <div>
+                    <h2 className='text-2xl mb-4'>Your booking information</h2>
+                    <BookingDates booking={booking} />
+                </div>
+                <div className='bg-primary px-8 py-2 rounded-2xl'>
+                    <div>  Total Price</div>
+                    <div className='text-2xl'>  ₹{booking.price}</div>
+                </div>
+            </div>
+        </div>
     )
 }
 
--- client/src/pages/HotelPage.jsx
+++ client/src/pages/HotelPage.jsx
@@ -5,6 +5,7 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faImage } from '@fortawesome/free-solid-svg-icons';
 import BookingWidget from '../components/BookingWidget';
 import PerksData from '../components/PerksData';
+import AddressLink from '../components/AddressLink';
 
 
 const HotelPage = () => {
@@ -69,14 +70,7 @@ const HotelPage = () => {
     return (
         <div className='mt-4 bg-gray-100 -mx-8 px-8 pt-8'>
             <h1 className='text-2xl'>{place.title}</h1>
-            <a className='flex gap-1 my-2  font-semibold underline' target='_blank' href={'https://maps.google.com/?q=' + place.address}>
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
-                    <path strokeLinecap="round" strokeLinejoin="round" d="M15 10.5a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
-                    <path strokeLinecap="round" strokeLinejoin="round" d="M19.5 10.5c0 7.142-7.5 11.25-7.5 11.25S4.5 17.642 4.5 10.5a7.5 7.5 0 1 1 15 0Z" />
-                </svg>
-
-                {place.address}
-            </a>
+            <AddressLink >{place.address}</AddressLink>
             <div className="relative">
                 <div className="grid gap-2 grid-cols-[2fr_1fr] rounded-3xl overflow-hidden">
                     <div>
